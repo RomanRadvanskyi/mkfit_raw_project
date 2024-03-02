20ca95a887421bab47de36cb62f38272b9429a34
romr2 <kopatelroma@gmail.com>
2024-03-02 11:26:38 +0100

    zaloha 4

--- resources/views/web/component/uvod/uvod-servicespage.blade.php
+++ resources/views/web/component/uvod/uvod-servicespage.blade.php
@@ -1,5 +1,5 @@
 <section class="container_uvod">
-    <div class="background-image"></div>
+    <div class="servicespage-background-image"></div>
     <img src="{{asset('resources/img/photos/hexagon_black.png')}}" alt="hexagon" class="hexagon">
     <div class="uvod1">
         <h1 class="title1">Služby</h1>
