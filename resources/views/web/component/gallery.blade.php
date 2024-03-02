4f8bb973b496941efd3ab8359a3704ca85ef4795
romr2 <kopatelroma@gmail.com>
2024-03-02 19:54:32 +0100

    zaloha 7

--- resources/views/web/component/gallery.blade.php
+++ resources/views/web/component/gallery.blade.php
@@ -2,22 +2,22 @@
 
     <div class="gallery_container" id="limitedGallery">
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo2.jpg')}}" data-lightbox="gallery" data-title="Kardio zariadenia">
+                <img src="{{asset('resources/img/photos/photo2.jpg')}}" alt="Kardio zariadenia">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo2.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo2.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo3.jpg')}}" data-lightbox="gallery" data-title="Posilňovací stroj">
+                <img src="{{asset('resources/img/photos/photo3.jpg')}}" alt="Posilňovací stroj">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo3.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo3.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo4.jpg')}}" data-lightbox="gallery" data-title="Posilňovacía veža">
+                <img src="{{asset('resources/img/photos/photo4.jpg')}}" alt="Posilňovacía veža">
                 <div class="overlay"></div>
             </a>
         </div>
@@ -25,70 +25,70 @@
 
     <div class="gallery_container" id="fullGallery" style="display: none;">
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo2.jpg')}}" data-lightbox="gallery" data-title="Kardio zariadenia">
+                <img src="{{asset('resources/img/photos/photo2.jpg')}}" alt="Kardio zariadenia">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo3.jpg')}}" data-lightbox="gallery" data-title="Posilňovací stroj">
+                <img src="{{asset('resources/img/photos/photo3.jpg')}}" alt="Posilňovací stroj">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo4.jpg')}}" data-lightbox="gallery" data-title="Posilňovacía veža">
+                <img src="{{asset('resources/img/photos/photo4.jpg')}}" alt="Posilňovacía veža">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo6.jpg')}}" data-lightbox="gallery" data-title="Výber činiek">
+                <img src="{{asset('resources/img/photos/photo6.jpg')}}" alt="Výber činiek">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo5.jpg')}}" data-lightbox="gallery" data-title="Naši zamestnanci a Silvester">
+                <img src="{{asset('resources/img/photos/photo5.jpg')}}" alt="Naši zamestnanci a Silvester">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo16.jpg')}}" data-lightbox="gallery" data-title="MK FIT merch">
+                <img src="{{asset('resources/img/photos/photo16.jpg')}}" alt="MK FIT merch">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Vysoko kvalitné cvičebné zariadenia">
+                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Vysoko kvalitné cvičebné zariadenia">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo14.jpg')}}" data-lightbox="gallery" data-title="MK FIT vybavenie">
+                <img src="{{asset('resources/img/photos/photo14.jpg')}}" alt="MK FIT vybavenie">
                 <div class="overlay"></div>
             </a>
         </div>
 
         <div class="gallery_item">
-            <a href="{{asset('resources/img/photos/photo1.jpg')}}" data-lightbox="gallery" data-title="Image 1">
-                <img src="{{asset('resources/img/photos/photo1.jpg')}}" alt="Image 1">
+            <a href="{{asset('resources/img/photos/photo15.jpg')}}" data-lightbox="gallery" data-title="MK FIT vybavenie">
+                <img src="{{asset('resources/img/photos/photo15.jpg')}}" alt="MK FIT vybavenie">
                 <div class="overlay"></div>
             </a>
         </div>
     </div>
 
-    <button class="show-all-button" onclick="toggleGallery()">Show All Images</button>
+    <button class="show-all-button" onclick="toggleGallery()">Zobraziť všetko</button>
 
     <!-- Lightbox JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox-plus-jquery.min.js"></script>
@@ -107,9 +107,9 @@
             fullGallery.toggle();
 
             if (fullGallery.is(":visible")) {
-                showAllButton.text("Show Less");
+                showAllButton.text("Zobraziť menej");
             } else {
-                showAllButton.text("Show All Images");
+                showAllButton.text("Zobraziť všetko");
             }
             updateAfterPseudoElement(); // Call the function to update ::after visibility
         }
